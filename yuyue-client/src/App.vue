d76bce4072a8b3110ab772a779764cc2b66c7124
indation_elina_guo <elina.guo@indation.com>
2019-03-03 02:31:05 +0800

    Save wechat to store

--- yuyue-client/src/App.vue
+++ yuyue-client/src/App.vue
@@ -22,7 +22,7 @@ export default {
       loadWechatInfo({code: urlParams.code}).then(res => {
         this.setWechatInfo(res)
         alert('set wechat info---')
-        alert('wechatInfo:' + JSON.stringify(this.wechatInfo))
+        alert(JSON.stringify(this.wechatInfo))
       }, err => {
         alert('get wechat info faild:' + err)
       })
