d1339509a0973b0964be197da366cf77d5d5dc2e
indation_elina_guo <elina.guo@indation.com>
2019-03-04 00:32:04 +0800

    Remove images

--- yuyue-client/src/pages/department/Department.vue
+++ yuyue-client/src/pages/department/Department.vue
@@ -6,54 +6,6 @@
           <img :src="item.thumb" >
           <span>{{item.name}}</span>
         </wv-grid-item>
-        <!-- <wv-grid-item>
-          <img src="../../assets/images/department/呼吸内科.png" >
-          <span>呼吸内科</span>
-        </wv-grid-item> -->
-        <!-- <wv-grid-item>
-          <img src="../../assets/images/department/康复科.png" >
-          <span>康复科</span>
-        </wv-grid-item>
-        <wv-grid-item>
-          <img src="../../assets/images/department/心内科.png" >
-          <span>心内科</span>
-        </wv-grid-item>
-        <wv-grid-item>
-          <img src="../../assets/images/department/急诊科.png" >
-          <span>急诊科</span>
-        </wv-grid-item>
-        <wv-grid-item>
-          <img src="../../assets/images/department/普外科.png" >
-          <span>普外科</span>
-        </wv-grid-item>
-        <wv-grid-item>
-          <img src="../../assets/images/department/检验科.png" >
-          <span>检验科</span>
-        </wv-grid-item>
-        <wv-grid-item>
-          <img src="../../assets/images/department/消化内科.png" >
-          <span>消化内科</span>
-        </wv-grid-item>
-        <wv-grid-item>
-          <img src="../../assets/images/department/眼科.png" >
-          <span>眼科</span>
-        </wv-grid-item>
-        <wv-grid-item>
-          <img src="../../assets/images/department/神经外科.png" >
-          <span>神经外科</span>
-        </wv-grid-item>
-        <wv-grid-item>
-          <img src="../../assets/images/department/空勤体检.png" >
-          <span>空勤体检</span>
-        </wv-grid-item>
-        <wv-grid-item>
-          <img src="../../assets/images/department/耳鼻喉科.png" >
-          <span>耳鼻喉科</span>
-        </wv-grid-item>
-        <wv-grid-item>
-          <img src="../../assets/images/department/骨科.png" >
-          <span>骨科</span>
-        </wv-grid-item> -->
       </wv-grid>
     </div>
   </div>
