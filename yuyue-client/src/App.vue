56237bfe299a27a4635e507d90b460ec2d723460
indation_elina_guo <elina.guo@indation.com>
2019-04-18 17:16:20 +0800

    Add test

--- yuyue-client/src/App.vue
+++ yuyue-client/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <div v-wechat-title="$route.meta.title"></div>
-    <router-view/>
+    <keep-alive>
+      <router-view v-if="$route.meta.keepAlive"/>
+    </keep-alive>
   </div>
 </template>
 
