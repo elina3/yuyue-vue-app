a4655e009388f01eb32acb75e864a51b165b2327
indation_elina_guo <elina.guo@indation.com>
2019-04-25 23:24:43 +0800

    Fix bug

--- yuyue-client/src/App.vue
+++ yuyue-client/src/App.vue
@@ -7,43 +7,43 @@
 
 <script>
 import { mapMutations } from 'vuex'
-// import { getCode } from '@/common/wechatsdk'
-// import { loadWechatInfo } from '@/services/wechat'
-// import { checkMemberInfo } from '@/services/member'
+import { getCode } from '@/common/wechatsdk'
+import { loadWechatInfo } from '@/services/wechat'
+import { checkMemberInfo } from '@/services/member'
 export default {
   name: 'App',
   // 在mounted阶段通过cookie拿到用户的userid
   created () {
-    // if (this.$store.state.wechatInfo) { // 重复返回首页时不再请求数据
-    //   return
-    // }
+    if (this.$store.state.wechatInfo) { // 重复返回首页时不再请求数据
+      return
+    }
 
-    // var urlParams = this.getUrlParmas()
-    // if (!urlParams.code) { // 去授权获取code
-    //   // alert('no code!')
-    //   let url = window.location.href
-    //   getCode(url)
-    // } else {
-    //   // alert('has code')
-    //   loadWechatInfo({code: urlParams.code}).then(res => {
-    //     if (res.wechat_info) {
-    //       // alert('wechatInfo:', JSON.stringify(res.wechat_info))
-    //       this.setWechatInfo(res.wechat_info)
-    //       // alert('app:' + JSON.stringify(this.$store.state.wechatInfo))
-    //       checkMemberInfo({open_id: res.wechat_info.openid}).then(res => {
-    //         // alert('result:', res)
-    //         if (res.member) {
-    //           // alert('has member')
-    //           this.setMemberInfo(res.member)
-    //         } else {
-    //           // alert('no member')
-    //         }
-    //       }, err => {
-    //         console.error(err)
-    //       })
-    //     }
-    //   })
-    // }
+    var urlParams = this.getUrlParmas()
+    if (!urlParams.code) { // 去授权获取code
+      // alert('no code!')
+      let url = window.location.href
+      getCode(url)
+    } else {
+      // alert('has code')
+      loadWechatInfo({code: urlParams.code}).then(res => {
+        if (res.wechat_info) {
+          // alert('wechatInfo:', JSON.stringify(res.wechat_info))
+          this.setWechatInfo(res.wechat_info)
+          // alert('app:' + JSON.stringify(this.$store.state.wechatInfo))
+          checkMemberInfo({open_id: res.wechat_info.openid}).then(res => {
+            // alert('result:', res)
+            if (res.member) {
+              // alert('has member')
+              this.setMemberInfo(res.member)
+            } else {
+              // alert('no member')
+            }
+          }, err => {
+            console.error(err)
+          })
+        }
+      })
+    }
   },
   methods: {
     ...mapMutations({
