ba041c42b8176fdbbf6cfbf3e7d570966b381469
indation_elina_guo <elina.guo@indation.com>
2019-05-16 11:14:53 +0800

    Modify the hospital location

--- yuyue-client/src/pages/home/Home.vue
+++ yuyue-client/src/pages/home/Home.vue
@@ -45,7 +45,11 @@
           <span>门诊报告</span>
         </wv-grid-item>
 
-        <wv-grid-item url="https://map.baidu.com/mobile/webapp/place/detail/qt=ninf&wd=%E6%B0%91%E8%88%AA%E4%B8%8A%E6%B5%B7%E5%8C%BB%E9%99%A2(%E7%91%9E%E9%87%91%E5%8F%A4%E5%8C%97%E5%88%86%E9%99%A2)&c=289&searchFlag=bigBox&version=4&src_from=webapp_all_bigbox&wd2=%E4%B8%8A%E6%B5%B7%E5%B8%82%E9%95%BF%E5%AE%81%E5%8C%BA&sug_forward=a74481d61b52415ff503b85d&src=1&uid=a74481d61b52415ff503b85d&industry=hospital&qid=8713050396733245199/showall=1&pos=0&da_ref=listclk&da_qrtp=11&da_adtitle=%E6%B0%91%E8%88%AA%E4%B8%8A%E6%B5%B7%E5%8C%BB%E9%99%A2(%E7%91%9E%E9%87%91%E5%8F%A4%E5%8C%97%E5%88%86%E9%99%A2)&vt=map/?fromhash=1">
+        <!-- <wv-grid-item url="https://map.baidu.com/mobile/webapp/place/detail/qt=ninf&wd=%E6%B0%91%E8%88%AA%E4%B8%8A%E6%B5%B7%E5%8C%BB%E9%99%A2(%E7%91%9E%E9%87%91%E5%8F%A4%E5%8C%97%E5%88%86%E9%99%A2)&c=289&searchFlag=bigBox&version=4&src_from=webapp_all_bigbox&wd2=%E4%B8%8A%E6%B5%B7%E5%B8%82%E9%95%BF%E5%AE%81%E5%8C%BA&sug_forward=a74481d61b52415ff503b85d&src=1&uid=a74481d61b52415ff503b85d&industry=hospital&qid=8713050396733245199/showall=1&pos=0&da_ref=listclk&da_qrtp=11&da_adtitle=%E6%B0%91%E8%88%AA%E4%B8%8A%E6%B5%B7%E5%8C%BB%E9%99%A2(%E7%91%9E%E9%87%91%E5%8F%A4%E5%8C%97%E5%88%86%E9%99%A2)&vt=map/?fromhash=1">
+          <img src="../../assets/images/home/医院导航.png" >
+          <span>医院位置</span>
+        </wv-grid-item> -->
+        <wv-grid-item url="https://m.amap.com/search/mapview/__p=B00155IL5Z,31.198135,121.401188,%E7%91%9E%E9%87%91%E5%8C%BB%E9%99%A2%E5%8F%A4%E5%8C%97%E5%88%86%E9%99%A2,%E7%BA%A2%E5%AE%9D%E7%9F%B3%E8%B7%AF398%E5%8F%B7&src=app_share&callnative=1&callapp=0&poiid=B00155IL5Z">
           <img src="../../assets/images/home/医院导航.png" >
           <span>医院位置</span>
         </wv-grid-item>
