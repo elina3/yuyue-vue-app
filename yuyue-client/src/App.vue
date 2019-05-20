853395a6b5e0f306fc0d7d9b1f84a1931022ae9d
indation_elina_guo <elina.guo@indation.com>
2019-05-20 13:10:03 +0800

    Fix bug

--- yuyue-client/src/App.vue
+++ yuyue-client/src/App.vue
@@ -23,11 +23,11 @@ export default {
       let url = window.location.href
       getCode(url)
     } else {
-      // alert('has code')
+      alert('has code')
       loadWechatInfo({code: urlParams.code}).then(res => {
         if (res.wechat_info) {
           this.setWechatInfo(res.wechat_info)
-          // alert('app:' + JSON.stringify(this.$store.state.wechatInfo))
+          alert('app:' + JSON.stringify(this.$store.state.wechatInfo))
           // alert('openid:' + res.wechat_info.openid)
           checkMemberInfo({open_id: res.wechat_info.openid}).then(res => {
             // alert('result:', res)
